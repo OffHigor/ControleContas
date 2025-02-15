1b01199d76035fee25910de645e8cbc966397727
Higor Rodrigues <firehigor.rodrigues@gmail.com>
2025-02-14 23:29:08 -0300

    Criacao Da Tabela de Transacoes Estrutura visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './Receitas.jsx'
+import React from 'react'
 import './App.css'
-import BlocoReceitas from './Receitas.jsx'
+import { BlocoReceitas, Transacoes} from './Receitas'
 
 
 function Bloco(){
   return( <div className='BlocoTabela'>
+    <div className='Redirecionador'>
     <div className='Direcionador'>
     <BlocoReceitas nome="Saldo" imagem="./src/assets/react.svg" /> 
     <BlocoReceitas nome="Receita" imagem="./public/vite.svg"/>
     <BlocoReceitas nome="Despesa" imagem="./public/vite.svg"/>
-
+    </div>
+    <Transacoes/>
     </div>
   </div>
   )
--- src/Receitas.jsx
+++ src/Receitas.jsx
@@ -1,6 +1,6 @@
 import './Receitas.css'
 
-function BlocoReceitas ({nome, imagem}) {
+const BlocoReceitas = function({nome, imagem}) {
     return(
         <div className='BlocoReceitas'>
         <p className='tt'>{nome}</p>
@@ -8,4 +8,24 @@ function BlocoReceitas ({nome, imagem}) {
         </div>
     )
 }
-export default BlocoReceitas
+const Transacoes = function(){
+    return(
+        <div className='Teste'>
+            <input type="text" name="Titulo" id="" placeholder='Titulo' />
+            <input type="text" name="Descricao" id="" placeholder='Ex: Lanchonete' />
+            <input type="date" name="Data" id=""  />
+
+            <select id="options">
+            <option value="" disabled selected>Tipo da Transacao</option>
+            <option value="opcao1">Despesa</option>
+            <option value="opcao2">Receita</option>
+            </select>
+
+            <input type="value" name="Valor" id="" placeholder='R$ 200,00' />
+            
+        </div>
+    )
+}
+
+  
+export {BlocoReceitas, Transacoes}
