5a3a69ccf131430b90db79d90b78392dcb437152
Higor Rodrigues <firehigor.rodrigues@gmail.com>
2025-03-21 11:10:38 -0300

    Teste

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,33 @@ import {Logar} from './Login'
 
 function Bloco(){
   return( 
-    <div className='TelaInicial'>
-      <div className='BlocoLogin'>
-        <img className='ImagemLogo' src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fbr.freepik.com%2Ficones%2Fdinheiro&psig=AOvVaw03IDGS9HvVDwep49ERC0jM&ust=1742641521395000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMDH8OWDm4wDFQAAAAAdAAAAABAE" alt="" />
-        <form action="" method="get">
-        <div className='InputUsuario'>
-          <label htmlFor="caracter">Usuario:</label>
-          <input type="email" placeholder='Digite o Email do Usuario' className='Inputs' />
-        </div>
-        <div className='InputUsuario'>
-          <label htmlFor="caracter">Senha:</label>
-          <input type="email" placeholder='Digite o Email do Usuario' className='Inputs' />
-        </div>
-        <div className='EsqSenha'><a href="#">Esqueceu a senha?</a></div>
+  <div className='TelaInicial'>
+    <div className='BlocoLogin'>
+     <img className='ImagemLogo' src='./assets/LogoControle.png' alt="" />
+      <form action="" method="get">
+          <div className='Control'>
 
-        <input type="button" value="Entrar"/>
-        </form>
-        <a href="#"><input type="button" value="Registrar"/></a>
-      </div>
+              <div className='InputUsuario'>
+              <label htmlFor="caracter">Usuario:</label>
+              <input type="email" placeholder='Digite o Email do Usuario:' className='Inputs' />
+              </div>
+
+              <div className='InputUsuario'>
+              <label htmlFor="caracter">Senha:</label>
+              <input type="email" placeholder='Digite sua senha:' className='Inputs' />
+              </div>
+
+              <div className='EsqSenha'><a href="#">Esqueceu a senha?</a></div>
+
+              <input id='BtnEntrar' type="button" value="Entrar"/>
+          </div>
+      </form>
+      <input id='BtnRegistrar' type="button" value="Registrar"/>
+          
     </div>
+  </div>
+    
+    
   )
 }
 export default Bloco
