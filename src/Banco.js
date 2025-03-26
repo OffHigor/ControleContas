785629e3e54eac0e70361e531c3c68e95f01978c
Higor Rodrigues <firehigor.rodrigues@gmail.com>
2025-03-26 14:37:48 -0300

    Delete src/Banco.js

--- src/Banco.js
+++
@@ -1,9 +0,0 @@
-//Incluir Dependencia MySQL
-const mysql = require('mysql2');
-
-//Criar conexão com banco de dados MySQL
-const connection = mysql.createConnection({
-    host: 'localhost'
-    user: 'root'
-    database: 'teste'
-})
